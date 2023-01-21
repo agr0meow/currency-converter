e56d97b743e90d00d69792a64195ff50e655cf42
agr0meow <neketa.erma13@gmail.com>
2023-01-21 12:56:30 +0300

    converter
    converter

---
+++ src/main.js
@@ -0,0 +1,35 @@
+// объект данных
+const bet = {
+    R_USD: 0.014,
+    R_EURO: 0.013,
+   
+}
+// логика конвертации
+function converter (rub,current) {
+    if(!bet[current]) {
+        return console.error('ошибка');
+    }
+    return rub * bet[converter]
+}
+// выбор элемента
+const valueInput = document.querySelector('.converter__input');
+const selectUsdOrEuro = document.querySelector('.converter__currency-selector');
+const outputMessage = document.querySelector('.output')
+const Button = document.querySelector('.converter__button');
+// Добавление событий
+Button.addEventListener(('click'), (event) => {
+    if(selectUsdOrEuro.value == 'R_USD') {
+        outputMessage.textContent = valueInput.value * bet.R_USD +' $ '
+    }
+    else if (selectUsdOrEuro.value == 'R_EURO') {
+        outputMessage.textContent = valueInput.value * bet.R_EURO + ' €'
+
+    }
+    else if (selectUsdOrEuro.value == 'USD_R') {
+        outputMessage.textContent = valueInput.value / bet.R_USD + '₽'
+    }
+    else if (selectUsdOrEuro.value == 'EURO_R') {
+        outputMessage.textContent = valueInput.value / bet.R_EURO + '₽'
+    }
+})
+
